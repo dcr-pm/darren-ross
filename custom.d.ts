396129cdb601057c443865a58766d8bade956455
dcr-pm <darrenclaudross@gmail.com>
2025-07-27 22:42:07 -0400

    Add files via upload

---
+++ custom.d.ts
@@ -0,0 +1,10 @@
+
+import 'react';
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'elevenlabs-convai': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & { 'agent-id': string };
+    }
+  }
+}
