23d4fcaf362f52adcdc34018fe47a49a228f38a0
dcr-pm <darrenclaudross@gmail.com>
2025-08-04 21:53:58 -0400

    Update custom.d.ts

--- darren-ross.com/custom.d.ts
+++ darren-ross.com/custom.d.ts
@@ -1,10 +1,7 @@
+/// <reference types="react" />
 
-import 'react';
-
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      'elevenlabs-convai': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & { 'agent-id': string };
-    }
+declare namespace JSX {
+  interface IntrinsicElements {
+    'elevenlabs-convai': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & { 'agent-id': string };
   }
 }
